721448e987f18dbcadc8fa3450d6653ef8ced298
logicoffee <riemann1618@gmail.com>
2018-12-09 20:00:53 +0900

    Add encoding function in another direction

--- src/Data/Era/Encoding.hs
+++ src/Data/Era/Encoding.hs
@@ -2,8 +2,12 @@ module Data.Era.Encoding where
 import Data.Era.AD
 import Data.Era.JpEra
 
-toJpEra :: AD -> JpEra
-toJpEra = undefined
+toJpEra :: AD -> Maybe JpEra
+toJpEra (AD year)
+  | year >= firstYear Heisei = makeJpEra Heisei $ year - firstYear Heisei + 1
+  | year >= firstYear Showa  = makeJpEra Showa  $ year - firstYear Showa + 1
+  | year >= firstYear Taisho = makeJpEra Taisho $ year - firstYear Taisho + 1
+  | year >= firstYear Meiji  = makeJpEra Meiji  $ year - firstYear Meiji + 1
 
 fromJpEra :: JpEra -> AD
 fromJpEra (JpEra era_type year) = AD $ firstYear era_type + year - 1
--- test/Spec.hs
+++ test/Spec.hs
@@ -12,11 +12,15 @@ main = do
     , "encode from Showa  to AD" ~: encodeFromJpEraToAD Showa 64 ~?= Just (AD 1989)
     , "encode from Taisho to AD" ~: encodeFromJpEraToAD Taisho 9 ~?= Just (AD 1920)
     , "encode from Meiji  to AD" ~: encodeFromJpEraToAD Meiji 37 ~?= Just (AD 1904)
+    , "encode from AD to Heisei" ~: toJpEra (AD 1993) ~?= makeJpEra Heisei 5
+    , "encode from AD to Showa"  ~: toJpEra (AD 1927) ~?= makeJpEra Showa  2
+    , "encode from AD to Taisho" ~: toJpEra (AD 1920) ~?= makeJpEra Taisho 9
+    , "encode from AD to Meiji"  ~: toJpEra (AD 1900) ~?= makeJpEra Meiji 33
     ]
   return ()
 
 
-encodeFromJpEraToAD :: JpEraType -> Year -> Maybe AD
+encodeFromJpEraToAD :: JpEraType -> Word -> Maybe AD
 encodeFromJpEraToAD era_type year = do
   jp_era <- makeJpEra era_type year
   return $ fromJpEra jp_era
