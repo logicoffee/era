53933293928b3369b4b6a1cc9f5ce3cb57fd096f
logicoffee <riemann1618@gmail.com>
2018-12-11 23:48:33 +0900

    Add era function

--- src/Data/Era.hs
+++ src/Data/Era.hs
@@ -2,3 +2,17 @@ module Data.Era where
 import Data.Era.AD
 import Data.Era.JpEra
 import Data.Era.Encoding
+import Text.Read
+
+
+era :: [String] -> Either String String
+era [era_type_char, year_str] = do
+  era_type <- match era_type_char
+  year <- readEither year_str
+  jp_era <- makeJpEra era_type year
+  return . show . fromJpEra $ jp_era
+
+era [year_str] = do
+  year <- readEither year_str
+  jp_era <- toJpEra $ AD year 
+  return . show $ jp_era
--- src/Data/Era/Encoding.hs
+++ src/Data/Era/Encoding.hs
@@ -2,7 +2,7 @@ module Data.Era.Encoding where
 import Data.Era.AD
 import Data.Era.JpEra
 
-toJpEra :: AD -> Maybe JpEra
+toJpEra :: AD -> Either String JpEra
 toJpEra (AD year)
   | year >= firstYear Heisei = makeJpEra Heisei $ year - firstYear Heisei + 1
   | year >= firstYear Showa  = makeJpEra Showa  $ year - firstYear Showa + 1
